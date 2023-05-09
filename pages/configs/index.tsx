c16a94a9393af5a5993e0bedf1ef91d2c688d9da
zvshka <pushpurs@yandex.ru>
2023-05-09 15:24:54 +0300

    Some changes

--- pages/configs/index.tsx
+++ pages/configs/index.tsx
@@ -1,12 +1,16 @@
-import { Container, Stack } from '@mantine/core';
+import { Container, Grid } from '@mantine/core';
 import React from 'react';
-import { PageHeader } from '../../components/Layout';
+import { Block, PageHeader } from '../../components/Layout';
 
 export default function Configs() {
   return (
-    <Container size="xl">
+    <Container size="xl" sx={{ height: '100%' }}>
       <PageHeader title="Пользовательские сборки" />
-      <Stack />
+      <Grid columns={48} mt="md">
+        <Grid.Col span={12} sx={{ height: '100%' }}>
+          <Block sx={{ height: '45rem' }}>Filters</Block>
+        </Grid.Col>
+      </Grid>
     </Container>
   );
 }
