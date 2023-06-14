200d9a90849d4cb9e69a8e7c935faca0465ba161
zvshka <pushpurs@yandex.ru>
2023-06-14 14:00:44 +0300

    Bug fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -291,7 +291,7 @@ export default function HomePage() {
         )}
         <Grid columns={48}>
           <MediaQuery styles={{ display: 'none' }} largerThan="sm">
-            <Grid.Col sx={{ position: 'sticky', top: '6rem', zIndex: 3 }}>
+            <Grid.Col sx={{ position: 'sticky', top: '5.5rem', zIndex: 3 }}>
               <Block sx={{ border: '1px solid lightgrey' }}>
                 <Tabs allowTabDeactivation>
                   <Tabs.List grow>
